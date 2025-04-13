f0039748e42ce1fda970b12a0fbbd9b4ae9aea62
Caleb-Secrest <caleb.secrest@mymail.champlain.edu>
2025-04-13 19:53:07 -0400

    Connect 4 Logic & MiniMax AI
    Got the win/draw logic for Connect 4 implemented and made a minimax algorithm that appears to work. Will need to check to make sure though.

--- Connect4-AI/Assets/Scripts/Board.cs
+++ Connect4-AI/Assets/Scripts/Board.cs
@@ -4,36 +4,238 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Board : MonoBehaviour
 {
     public List<Cell> columns = new List<Cell>();
     public List<Image> buttons = new List<Image>();
+    public TMP_Text winText;
+    public MiniMaxAI ai;
+
+    public int[,] boardState = new int [7,6];
+
+    private int winner = 0;
+
+    private bool humanTurn = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Image i in buttons)
-        {
-            i.color = Color.red;
-        }
-    }
+        ButtonColor(Color.red);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void UpdateBoard()
-    {
-        for (int i = 0; i < columns.Count; i++)
+        for (int i = 0; i < boardState.GetLength(0); i++)
         {
-            for (int j = 0; j< columns[i].Count; i++)
+            for (int j = 0; j < boardState.GetLength(1); j++)
             {
-
+                boardState[i, j] = 0;
             }
         }
+    }
+
+    private void ButtonColor(Color color)
+    {
+        foreach(Image i in buttons)
+        {
+            i.color = color;
+        }
+    }    
+
+    private void UpdateBoard(int col, int row, int player)
+    {
+        columns[col].cells[row].GetComponent<CellValue>().value = player;
+
+        for (int i = 0; i < columns.Count; i++)
+        {
+            foreach (GameObject c in columns[i].cells)
+            {
+                switch (c.GetComponent<CellValue>().value)
+                {
+                    case 1:
+                        c.GetComponent<SpriteRenderer>().color = Color.red;
+                        break;
+                    case 2:
+                        c.GetComponent<SpriteRenderer>().color = Color.yellow;
+                        break;
+                    default:
+                        c.GetComponent<SpriteRenderer>().color = Color.white;
+                        break;
+                }
+            }
+        }
+
+        CheckWin();
+
+        switch (player)
+        {
+            case 1:
+                humanTurn = false;
+                ButtonColor(Color.yellow);
+                StartCoroutine(DelayedAIMove());
+                break;
+            case 2:
+                humanTurn = true;
+                ButtonColor(Color.red);
+                break;
+            default:
+                humanTurn = true;
+                ButtonColor(Color.red);
+                break;
+        }
+    }
+
+    private void CheckWin()
+    {
+        int width = boardState.GetLength(0);  // 7
+        int height = boardState.GetLength(1); // 6
+        bool isBoardFull = true;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int player = boardState[x, y];
+
+                // Check Draw
+                if (player == 0)
+                {
+                    isBoardFull = false;
+                    continue;
+                }
+
+                // Check Horizontal
+                if (x + 3 < width &&
+                    player == boardState[x + 1, y] && player == boardState[x + 2, y] && player == boardState[x + 3, y])
+                {
+                    EndGame(player);
+                    return;
+                }
+
+                // Check Vertical
+                if (y + 3 < height && player == boardState[x, y + 1] && player == boardState[x, y + 2] && player == boardState[x, y + 3])
+                {
+                    EndGame(player);
+                    return;
+                }
+
+                // Check Diagonal Down-Right
+                if (x + 3 < width && y + 3 < height && player == boardState[x + 1, y + 1] && player == boardState[x + 2, y + 2] && player == boardState[x + 3, y + 3])
+                {
+                    EndGame(player);
+                    return;
+                }
+
+                // Check Diagonal Up-Right
+                if (x + 3 < width && y - 3 >= 0 && player == boardState[x + 1, y - 1] && player == boardState[x + 2, y - 2] && player == boardState[x + 3, y - 3])
+                {
+                    EndGame(player);
+                    return;
+                }
+            }
+        }
+
+        if (isBoardFull)
+        {
+            EndGame(0);
+        }
+    }
+
+    private void EndGame(int winningPlayer)
+    {
+        winner = winningPlayer;
+        humanTurn = false;
+
+        switch(winner)
+        {
+            case 1:
+                winText.text = "PLAYER\nWINS!";
+                break;
+            case 2:
+                winText.text = "COMPUTER\nWINS!";
+                break;
+            default:
+                winText.text = "DRAW!";
+                break;
+        }
+
+        // Optionally disable input or show UI
+        foreach (Image i in buttons)
+        {
+            i.raycastTarget = false;
+        }
+        ButtonColor(Color.gray);
+    }
+
+    public void PlacePiece(int col, int player)
+    {
+        for (int i = 0; i < columns[col].cells.Count; i++)
+        {
+            if (columns[col].cells[i].GetComponent<CellValue>().value == 0)
+            {
+                boardState[col, i] = player;
+                UpdateBoard(col, i, player);
+                break;
+            }
+        }
+    }
+
+    IEnumerator DelayedAIMove()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ai.MakeAIMove();
+    }
+
+    public int[,] GetBoardState()
+    {
+        return boardState;
+    }
+
+    public void ColOne()
+    {
+        if (humanTurn)
+            PlacePiece(0, 1);
+    }
+
+    public void ColTwo()
+    {
+        if (humanTurn)
+            PlacePiece(1, 1);
+    }
+
+    public void ColThree()
+    {
+        if (humanTurn)
+            PlacePiece(2, 1);
+    }
+
+    public void ColFour()
+    {
+        if (humanTurn)
+            PlacePiece(3, 1);
+    }
+
+    public void ColFive()
+    {
+        if (humanTurn)
+            PlacePiece(4, 1);
+    }
+
+    public void ColSix()
+    {
+        if (humanTurn)
+            PlacePiece(5, 1);
+    }
+
+    public void ColSeven()
+    {
+        if (humanTurn)
+            PlacePiece(6, 1);
+    }
+
+    public void ResetBoard()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
 
--- Connect4-AI/Assets/Scripts/MiniMaxAI.cs
+++ Connect4-AI/Assets/Scripts/MiniMaxAI.cs
@@ -1,18 +1,151 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MiniMaxAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Board board;
+    public int maxDepth = 5;
+
+    public void MakeAIMove()
     {
-        
+        int bestCol = GetBestMove(board.GetBoardState(), maxDepth);
+        board.PlacePiece(bestCol, 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    int GetBestMove(int[,] boardState, int depth)
     {
-        
+        int bestScore = int.MinValue;
+        int bestCol = 0;
+
+        for (int col = 0; col < 7; col++)
+        {
+            int row = GetNextOpenRow(boardState, col);
+            if (row != -1)
+            {
+                int[,] tempBoard = (int[,])boardState.Clone();
+                tempBoard[col, row] = 2;
+
+                int score = MiniMax(tempBoard, depth - 1, false);
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestCol = col;
+                }
+            }
+        }
+
+        return bestCol;
+    }
+
+    int MiniMax(int[,] boardState, int depth, bool isMaximizing)
+    {
+        int winner = CheckWinner(boardState);
+        if (winner == 2) return 1000;
+        if (winner == 1) return -1000;
+        if (IsFull(boardState) || depth == 0) return EvaluateBoard(boardState);
+
+        if (isMaximizing)
+        {
+            int maxEval = int.MinValue;
+            for (int col = 0; col < 7; col++)
+            {
+                int row = GetNextOpenRow(boardState, col);
+                if (row != -1)
+                {
+                    int[,] tempBoard = (int[,])boardState.Clone();
+                    tempBoard[col, row] = 2;
+
+                    int eval = MiniMax(tempBoard, depth - 1, false);
+                    maxEval = Mathf.Max(maxEval, eval);
+                }
+            }
+            return maxEval;
+        }
+        else
+        {
+            int minEval = int.MaxValue;
+            for (int col = 0; col < 7; col++)
+            {
+                int row = GetNextOpenRow(boardState, col);
+                if (row != -1)
+                {
+                    int[,] tempBoard = (int[,])boardState.Clone();
+                    tempBoard[col, row] = 1;
+
+                    int eval = MiniMax(tempBoard, depth - 1, true);
+                    minEval = Mathf.Min(minEval, eval);
+                }
+            }
+            return minEval;
+        }
+    }
+
+    int EvaluateBoard(int[,] boardState)
+    {
+        int score = 0;
+
+        for (int x = 0; x < 7; x++)
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                if (boardState[x, y] == 2)
+                {
+                    score += 1;
+                }
+            }
+        }
+
+        return score;
+    }
+
+    int GetNextOpenRow(int[,] boardState, int col)
+    {
+        for (int y = 0; y < 6; y++)
+        {
+            if (boardState[col, y] == 0)
+                return y;
+        }
+        return -1;
+    }
+
+    bool IsFull(int[,] boardState)
+    {
+        for (int x = 0; x < 7; x++)
+        {
+            if (GetNextOpenRow(boardState, x) != -1)
+                return false;
+        }
+        return true;
+    }
+
+    int CheckWinner(int[,] boardState)
+    {
+        int width = boardState.GetLength(0);
+        int height = boardState.GetLength(1);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int player = boardState[x, y];
+                if (player == 0) continue;
+
+                // Horizontal
+                if (x + 3 < width && player == boardState[x + 1, y] && player == boardState[x + 2, y] && player == boardState[x + 3, y])
+                    return player;
+                // Vertical
+                if (y + 3 < height && player == boardState[x, y + 1] && player == boardState[x, y + 2] && player == boardState[x, y + 3])
+                    return player;
+                // Diagonal down-right
+                if (x + 3 < width && y + 3 < height && player == boardState[x + 1, y + 1] && player == boardState[x + 2, y + 2] && player == boardState[x + 3, y + 3])
+                    return player;
+                // Diagonal up-right
+                if (x + 3 < width && y - 3 >= 0 && player == boardState[x + 1, y - 1] && player == boardState[x + 2, y - 2] && player == boardState[x + 3, y - 3])
+                    return player;
+            }
+        }
+
+        return 0;
     }
 }
